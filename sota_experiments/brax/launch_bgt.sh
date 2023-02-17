c8fd5f544463f98a5960d364585c81e964fc6099
rraileanu <raileanu.roberta@gmail.com>
2023-02-17 06:13:23 -0800

    added wandb project

--- sota_experiments/brax/launch_bgt.sh
+++ sota_experiments/brax/launch_bgt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --array=0
+#SBATCH --array=2
 #SBATCH --error=./bgt.err
 #SBATCH --job-name=bgt_brax
 #SBATCH --mem=10GB
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=./bgt.out
-#SBATCH --partition=learnlab
+#SBATCH --partition=learnfair
 #SBATCH --time=2000
 
 python run_bgt.py --multirun seed=$SLURM_ARRAY_TASK_ID env_name=$1 num_timesteps=$2 hydra.launcher.partition=learnlab
--- sota_experiments/procgen/launch_bgt.sh
+++ sota_experiments/procgen/launch_bgt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --array=0-2
+#SBATCH --array=0
 #SBATCH --error=./bgt.err
 #SBATCH --job-name=bgt_procgen
 #SBATCH --mem=10GB
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=./bgt.out
-#SBATCH --partition=learnlab
+#SBATCH --partition=learnfair
 #SBATCH --time=2000
 
 python run_bgt.py --multirun seed=$SLURM_ARRAY_TASK_ID env_name=$1 clf_hidden_size=$2 hydra.launcher.partition=learnfair
